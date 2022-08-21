ce8fac19549fa6c655c78d9a6381981cee3ff55f
TGPBot <111167531+TGPBot@users.noreply.github.com>
2022-08-21 17:52:38 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -531,6 +531,5 @@ async def save_template(client, message):
 @Client.on_message(filters.private & filters.text & ~filters.regex("^/"))
 async def msg_handler(c, m):
     await m.reply_text(
-        "<b>⭕️👉 𝖱ᴇǫᴜᴇ𝗌ᴛ Mᴏᴠɪᴇ Hᴇʀᴇ 👈⭕️\n   ┃ㅤㅤㅤㅤㅤㅤㅤㅤ ㅤㅤㅤ          ┃\n
-⭕️👉 @PenMovieRequest '👈⭕️</b>"
+        "<b>⭕️👉 𝖱ᴇǫᴜᴇ𝗌ᴛ Mᴏᴠɪᴇ Hᴇʀᴇ\n⭕️👉 @PenMovieRequest</b>"
     )
